5e0efeabeda6d7ff1dd1c6ea463216329c842714
yashika2707 <yashikavelu@gmail.com>
2023-05-14 14:49:35 +0530

    Updated

---
+++ index1.js
@@ -0,0 +1,23 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "lavanyavelu1@gmail.com";
+    const templateID = "template_tsnxgje";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
