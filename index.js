f39630664b4f2a3a188cc9b6c9162fe79916eee5
yashika2707 <yashikavelu@gmail.com>
2023-05-15 11:53:59 +0530

    update code

--- index.js
+++ index.js
@@ -20,15 +20,3 @@ alert("Your message sent successfully");
 })
 .catch ((err) => console.log(err));
 }
-  function validatMessage(){
-    var message = document.getElementById("message").value;
-    var required = 30;
-    var left = required . message . length;
-
-    if(left>0){
-        messageError.innerHTML = left . 'more characters required' ;
-        return false;
-
-    }
-    emailError.innerHTML = "<i class="fa.fa.check.circle"></i>
-  }
