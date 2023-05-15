eda0b38cd6aa0da0ba9e54a551644e85463c936a
yashika2707 <yashikavelu@gmail.com>
2023-05-15 11:53:28 +0530

    initial push

--- index1.js
+++ index1.js
@@ -21,3 +21,18 @@ function sendMail() {
   
   }
   
+
+  
+
+$('.palceholder').click(function() {
+  $(this).siblings('input').focus();
+});
+$('.form-control').focus(function() {
+  $(this).siblings('.palceholder').hide();
+});
+$('.form-control').blur(function() {
+  var $this = $(this);
+  if ($this.val().length == 0)
+    $(this).siblings('.palceholder').show();
+});
+$('.form-control').blur();
