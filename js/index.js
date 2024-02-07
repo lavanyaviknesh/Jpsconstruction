1871fc2375d8ab69ec6c507aebc73d5b893ecf19
yashika2707 <yashikavelu@gmail.com>
2024-02-07 16:14:57 +0530

    update js mail

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ document.getElementById("email").value ="";
 document.getElementById("mobile").value ="";
 document.getElementById("message").value ="";
 console.log(res);
+alert("Your message sent successfully");
 })
 .catch ((err) => console.log(err));
 alert("Your message sent successfully");
