7d0521287a36c2d4365b16c15b2784a040b51b67
yashika2707 <yashikavelu@gmail.com>
2024-02-07 15:37:54 +0530

    updated html and js for mail

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
-function sendMail() 
-				{
-			var templateParams = {
-				name: document.getElementById("name").value,
-				location: document.getElementById("yourlocation").value,
-				email: document.getElementById("email").value,
-				mobile: document.getElementById("mobile").value,
-				message: document.getElementById("message").value,
-			};
-			emailjs.init("HVA9yqdwWWcIxL5_k");
-			emailjs.send("service_wacsyd4","template_0oyajwd", templateParams).then(
-        res=>{
-    document.getElementById("name").value ="" ;
-    document.getElementById("email").value ="";
-    document.getElementById("message").value ="";
-    console.log(res);
-    alert("Your message sent successfully");
-    })
-    .catch ((err) => console.log(err));
-    alert("Your message sent successfully");
-				}
-
+function sendMail() {
+	var params = {
+	name: document.getElementById("name").value,
+	location: document.getElementById("yourlocation").value,
+	email: document.getElementById("email").value,
+	mobile: document.getElementById("mobile").value,
+	message: document.getElementById("message").value,
+	};
+	const serviceID = "service_wacsyd4";
+	const templateID = "template_0oyajwd";
+	emailjs.send(serviceID, templateID, params)
+	.then(res=>{
+	document.getElementById("name").value ="" ;
+	document.getElementById("yourlocation").value ="";
+	document.getElementById("email").value ="";
+	document.getElementById("mobile").value ="";
+	document.getElementById("message").value ="";
+	console.log(res);
+	alert("Your message sent successfully");
+	})
+	.catch ((err) => console.log(err));
+	alert("Your message sent successfully");
+	}
 	
-        
+					
+	
